5fa86013e77f7a269c501bf90bc65645538e4733
Eva Haugrund <58939941+EvaHaugrund@users.noreply.github.com>
2020-01-20 10:34:41 +0100

    hit und colision hinzugefügt

--- js/JumpNRun.js
+++ js/JumpNRun.js
@@ -20,7 +20,7 @@ var dog = {
   walking: false,
   direction: 1
 };
-let dogStanding = [];
+// let dogStanding = [];
 
 function drawPlayer() {
   //rect(dog.x, dog.y, dog.sx, dog.sy);
@@ -108,36 +108,48 @@ function movePlayer() {
   } else {
     dog.peeing = false;
   }
-
   dog.y_velocity += 1.5; // gravity
+  for (var i in page.shrubbery) {
+    if (colision(dog, page.shrubbery[i])) {
+      dog.x_velocity = 0;
+      dog.t++;
+    } else {
+      dog.y += dog.y_velocity;
+      if (colision(dog, page.shrubbery[i])) {
+        dog.y_velocity = 0;
+        dog.jumping = false;
+        dog.y = page.shrubbery[i].y - dog.sy - 1;
+      } else {
+        dog.y -= dog.y_velocity;
+      }
+    }
+  }
+
   dog.x += dog.x_velocity;
   dog.y += dog.y_velocity;
   dog.x_velocity *= 0.9; // friction
   dog.y_velocity *= 0.9; // friction
 
   // if dog is falling below floor line
-  // if (dog.x >= hole.x && dog.x + dog.sx <= hole.x + hole.sx) {
-  //   fall = true;
-  // } else {
-  //   fall = false;
-  // }
+  for (let i in page.holes) {
+    if (
+      dog.x >= page.holes[i].x &&
+      dog.x + dog.sx <= page.holes[i].x + page.holes[i].sx
+    ) {
+      dog.fall = true;
+    } else {
+      dog.fall = false;
+    }
+  }
   if (dog.y > 465 && dog.fall === false) {
     dog.jumping = false;
     dog.y = 465;
     dog.y_velocity = 0;
   }
-  // else if (
-  //   dog.y + dog.sy >= shrub.y &&
-  //   dog.x + dog.sx - 2 >= shrub.x &&
-  //   dog.x <= shrub.x + shrub.sx - 2
-  // ) {
-  //   dog.jumping = false;
-  //   dog.y = 440;
-  //   dog.y_velocity = 0;
-  // }
-  // if (dog.y > 600) {
-  //   state = gameOver;
-  // }
+
+  if (dog.y > 600) {
+    state = 2;
+  }
 
   //if dog is leaving canvas
   if (dog.x > 640) {
@@ -145,20 +157,22 @@ function movePlayer() {
     pageNow += 1;
     switchPage(pageNow);
   } else if (dog.x < -1 && pageNow >= 1) {
-    dog.x = 630;
-    pageNow -= 1;
-    switchPage(pageNow);
+    if (pageNow != 0 && pageNow != 3 && pageNow != 9) {
+      dog.x = 630;
+      pageNow -= 1;
+      switchPage(pageNow);
+    }
   }
-  if (dog.x <= 0 && pageNow === 0) {
+  if (dog.x <= 0 && (pageNow === 0 || pageNow === 3 || pageNow === 9)) {
     dog.x = 0;
   }
 }
 
 //Cats
 function spawnCat(cx, cy, sp) {
-  var cat = {
+  let cat = {
     startX: cx,
-    x: 0,
+    x: cx,
     y: cy,
     picX: 0,
     picY: 0,
@@ -166,7 +180,7 @@ function spawnCat(cx, cy, sp) {
     direction: 1,
     sx: 30,
     sy: 28,
-    dx: 200,
+    dx: 400,
     t: 0,
     walking: false,
     attacking: false
@@ -174,7 +188,7 @@ function spawnCat(cx, cy, sp) {
   return cat;
 }
 function drawCat(cat) {
-  //rect(cat.x, cat.y, cat.sx, cat.sy);
+  // rect(cat.x, cat.y, cat.sx, cat.sy);
   if (cat.walking === true) {
     cat.picX = 0;
     cat.picY = 0;
@@ -211,6 +225,19 @@ function drawCat(cat) {
   }
 }
 function moveCat(cat) {
+  for (let i in page.shrubbery) {
+    if (colision(cat, page.shrubbery[i])) {
+      cat.direction = cat.direction * -1;
+    }
+  }
+  for (let i in page.holes) {
+    if (
+      cat.x + cat.sx >= page.holes[i].x &&
+      cat.x <= page.holes[i].x + page.holes[i].sx
+    ) {
+      cat.direction = cat.direction * -1;
+    }
+  }
   if (
     (cat.x < cat.dx && cat.direction === 1) ||
     (cat.direction === -1 && cat.x > 0)
@@ -230,7 +257,7 @@ function moveCat(cat) {
 function spawnHuman(cx, cy, sp) {
   var human = {
     startX: cx,
-    x: 100,
+    x: cx,
     y: cy,
     picX: 0,
     picY: 0,
@@ -238,7 +265,7 @@ function spawnHuman(cx, cy, sp) {
     direction: 1,
     sx: 30,
     sy: 67,
-    dx: 200,
+    dx: 600,
     t: 0,
     walking: false
   };
@@ -288,6 +315,19 @@ function drawHuman(human) {
   }
 }
 function moveHuman(human) {
+  for (let i in page.shrubbery) {
+    if (colision(human, page.shrubbery[i])) {
+      human.direction = human.direction * -1;
+    }
+  }
+  for (let i in page.holes) {
+    if (
+      human.x + human.sx >= page.holes[i].x &&
+      human.x <= page.holes[i].x + page.holes[i].sx
+    ) {
+      human.direction = human.direction * -1;
+    }
+  }
   if (
     (human.x < human.dx && human.direction === 1) ||
     (human.direction === -1 && human.x > 0)
@@ -304,12 +344,13 @@ function moveHuman(human) {
 }
 
 //Trees
-function spawnTree(cx, cy) {
+function spawnTree(cx, cy, h) {
   var tree = {
     x: cx,
     y: cy,
     sx: 60,
-    sy: 125
+    sy: 125,
+    hit: h
   };
   return tree;
 }
@@ -323,66 +364,120 @@ function spawnShrub(cx, cy) {
   var shrub = {
     x: cx,
     y: cy,
-    sx: 50,
-    sy: 40
+    sx: 48,
+    sy: 50
   };
   return shrub;
 }
 function drawShrub(shrub) {
-  rect(shrub.x, shrub.y, shrub.sx, shrub.sy);
+  //rect(shrub.x, shrub.y, shrub.sx, shrub.sy);
+  image(shrubPNG, shrub.x - 2, shrub.y - 8, 220, 300, 70, 140, 800, 800);
+}
+
+//hole
+function spawnHole(cx, cy) {
+  var hole = {
+    x: cx,
+    y: cy,
+    sx: 70,
+    sy: 150
+  };
+  return hole;
+}
+function drawHole(hole) {
+  rect(hole.x, hole.y, hole.sx, hole.sy);
+  image(holePNG, hole.x, hole.y, hole.sx, hole.sy, 0, 0, 800, 800);
+}
+
+//live Box
+function spawnLiveBox(cx, cy, h) {
+  let box = {
+    x: cx,
+    y: cy,
+    sx: 20,
+    sy: 20,
+    hit: h
+  };
+  return box;
+}
+function drawLiveBox(box) {
+  //rect(box.x, box.y, box.sx, box.sy);
+  image(liveBoxPNG, box.x, box.y, 21, 21, 0, 0, 21, 21);
+}
+
+//doghouse
+function spawnDoghouse(cx, cy) {
+  let house = {
+    x: cx,
+    y: cy,
+    sx: 74,
+    sy: 72
+  };
+  return house;
+}
+function drawDoghouse(house) {
+  //rect(house.x, house.y, house.sx, house.sy);
+  image(doghousePNG, house.x - 1, house.y, 220, 300, 65, 130, 800, 800);
+}
+
+//text
+function explained(cx, xy) {
+  fill(0, 0, 0);
+  text("you walk and jump with the arrows", cx, cy);
 }
 
 //colision
 function hit() {
-  if (colision(cat, dog) === true && (gameCanvas === 1 || gameCanvas === 3)) {
-    dog.live -= 1;
+  for (var i in page.cats) {
+    if (colision(dog, page.cats[i]) === true) {
+      dog.live -= 1;
+    }
   }
-  if (colision(box, dog) === true && lUp === false) {
-    dog.live += 1;
-    lUp = true;
+  for (var b in page.liveBox) {
+    if (colision(page.liveBox[b], dog) === true) {
+      dog.live += 1;
+    }
   }
-  if (colision(dog, human) === true && gameCanvas === 2) {
-    canvas = gameOver;
+
+  for (var j in page.humans) {
+    if (colision(dog, page.humans[j]) === true) {
+      state = 2;
+    }
   }
-  if (colision(dog, tree) === true && dogPeeing === true && pUp === false) {
-    dog.points += 1;
-    dogPeeing = false;
-    pUp = true;
-  } else if (colision(dog, tree) === false && dogPeeing === true) {
-    dogPeeing = false;
+
+  for (var t in page.trees) {
+    if (colision(dog, page.trees[t]) === true && dog.peeing === true) {
+      dog.points += 1;
+      dog.peeing = false;
+    }
   }
-  if (
-    dog.x + dog.sx >= shrub.x &&
-    dog.x <= shrub.x + 1 &&
-    dog.y + dog.sy >= shrub.y
-  ) {
-    dog.x = shrub.x - dog.sx;
-  } else if (
-    dog.x <= shrub.x + shrub.sx &&
-    dog.x >= shrub.x + shrub.sx - 5 &&
-    dog.y + dog.sy >= shrub.y
-  ) {
-    dog.x = shrub.x + shrub.sx;
+  for (let i in page.doghouse) {
+    if (
+      colision(dog, page.doghouse[i]) === true &&
+      dog.x + 20 >= page.doghouse[i].x
+    ) {
+      state = 3;
+    }
   }
 }
 function colision(obj1, obj2) {
   if (
-    (obj1.x > obj2.x &&
-      obj1.x < obj2.x + obj2.sx &&
-      obj1.y > obj2.y &&
-      obj1.y < obj2.y + obj2.sy) ||
-    (obj1.x + obj1.sx > obj2.x &&
-      obj1.x + obj1.sx < obj2.x + obj2.sx &&
-      obj1.y > obj2.y &&
-      obj1.y < obj2.y + obj2.sy) ||
-    (obj1.x + obj1.sx > obj2.x &&
-      obj1.x + obj1.sx < obj2.x + obj2.sx &&
-      obj1.y + obj1.sy > obj2.y &&
-      obj1.y + obj1.sy < obj2.y + obj2.sy) ||
-    (obj1.x > obj2.x &&
-      obj1.x < obj2.x + obj2.sx &&
-      obj1.y + obj1.sy > obj2.y &&
-      obj1.y + obj1.sy < obj2.y + obj2.sy)
+    (obj1.x >= obj2.x &&
+      obj1.x <= obj2.x + obj2.sx &&
+      obj1.y >= obj2.y &&
+      obj1.y <= obj2.y + obj2.sy) ||
+    (obj1.x + obj1.sx >= obj2.x &&
+      obj1.x + obj1.sx <= obj2.x + obj2.sx &&
+      obj1.y >= obj2.y &&
+      obj1.y <= obj2.y + obj2.sy) ||
+    (obj1.x + obj1.sx >= obj2.x &&
+      obj1.x + obj1.sx <= obj2.x + obj2.sx &&
+      obj1.y + obj1.sy >= obj2.y &&
+      obj1.y + obj1.sy <= obj2.y + obj2.sy) ||
+    (obj1.x >= obj2.x &&
+      obj1.x <= obj2.x + obj2.sx &&
+      obj1.y + obj1.sy >= obj2.y &&
+      obj1.y + obj1.sy <= obj2.y + obj2.sy)
   ) {
     return true;
   }
@@ -390,19 +485,36 @@ function colision(obj1, obj2) {
 }
 
 //canvas
+let state = 0;
 let level = {
   page: []
 };
 let page = {};
 let pageNow = 0;
+
+function drawLive() {
+  fill(0, 0, 0);
+  textSize(15);
+  text("Live:" + dog.live, 50, 50);
+
+  if (dog.live <= 0) {
+    state = 2;
+  }
+}
+function drawPoints() {
+  fill(0, 0, 0);
+  textSize(15);
+  text("Points:" + dog.points, 50, 70);
+}
+
 function start() {
   fill(100, 100, 100);
   rect(200, 200, 100, 50);
   rect(400, 200, 100, 50);
   fill(0, 0, 0);
   textSize(15);
-  text("Start Game", 220, 230);
-  text("Select Level", 415, 230);
+  text("Start Game", 215, 230);
+  text("Select Level", 410, 230);
 }
 function selectLevel() {
   fill(100, 100, 100);
@@ -415,7 +527,7 @@ function selectLevel() {
   text("Level 1", 330, 170);
   text("Level 2", 330, 230);
   text("Level 3", 330, 290);
-  text("back", 513, 116);
+  text("back", 510, 118);
 }
 function gameOver() {
   fill(200, 200, 200);
@@ -430,50 +542,296 @@ function gameOver() {
   text("restart", 390, 290);
   dog.points = 0;
 }
+function gameWon() {
+  fill(200, 200, 200);
+  textSize(30);
+  text("Game Won", 270, 250);
+  fill(100, 100, 100);
+  rect(240, 260, 100, 50);
+  rect(360, 260, 100, 50);
+  fill(0, 0, 0);
+  textSize(15);
+  text("back to Menu", 247, 290);
+  text("next level", 380, 290);
+}
+function drawLevelNr() {
+  let number;
+  if (pageNow <= 2) {
+    number = 1;
+  } else if (pageNow >= 3 && pageNow <= 8) {
+    number = 2;
+  } else if (pageNow >= 9) {
+    number = 3;
+  }
+  text("Level:" + number, 550, 50);
+}
+
+function mousePressed() {
+  //Start Game
+  if (
+    state === 0 &&
+    mouseX > 200 &&
+    mouseX < 300 &&
+    mouseY > 200 &&
+    mouseY < 250
+  ) {
+    state = 4;
+    dog.x = 0;
+    switchPage(pageNow);
+  }
+  //Level selection menu
+  if (
+    state === 0 &&
+    mouseX > 400 &&
+    mouseX < 500 &&
+    mouseY > 200 &&
+    mouseY < 250
+  ) {
+    state = 1;
+  }
+  //select Level 1
+  if (
+    state === 1 &&
+    mouseX > 300 &&
+    mouseX < 400 &&
+    mouseY > 140 &&
+    mouseY < 190
+  ) {
+    pageNow = 0;
+    console.log("Level 1");
+  }
+  //select Level 2
+  if (
+    state === 1 &&
+    mouseX > 300 &&
+    mouseX < 400 &&
+    mouseY > 200 &&
+    mouseY < 250
+  ) {
+    pageNow = 3;
+    switchPage(pageNow);
+    console.log("Level 2");
+  }
+  //select Level 3
+  if (
+    state === 1 &&
+    mouseX > 300 &&
+    mouseX < 400 &&
+    mouseY > 260 &&
+    mouseY < 310
+  ) {
+    pageNow = 9;
+    switchPage(pageNow);
+    console.log("Level 3");
+  }
+  //back to start
+  if (
+    state === 1 &&
+    mouseX > 500 &&
+    mouseX < 550 &&
+    mouseY > 100 &&
+    mouseY < 125
+  ) {
+    state = 0;
+    console.log("back");
+  }
+  //game over
+  //back to start
+  if (
+    state === 2 &&
+    mouseX > 240 &&
+    mouseX < 340 &&
+    mouseY > 260 &&
+    mouseY < 310
+  ) {
+    state = 0;
+    dog.live = 100;
+    dog.points = 0;
+    dog.x = 0;
+    if (pageNow <= 3) {
+      pageNow = 0;
+      switchPage(pageNow);
+    } else if (pageNow >= 4 && pageNow <= 8) {
+      pageNow = 4;
+      switchPage(pageNow);
+    } else if (pageNow >= 9 && pageNow <= 14) {
+      pageNow = 9;
+      switchPage(pageNow);
+    }
+    console.log("back to Menu");
+  }
+  //restart level
+  if (
+    state === 2 &&
+    mouseX > 360 &&
+    mouseX < 460 &&
+    mouseY > 260 &&
+    mouseY < 310
+  ) {
+    state = 4;
+    dog.live = 100;
+    dog.x = 0;
+    if (pageNow <= 2) {
+      pageNow = 0;
+      switchPage(pageNow);
+    } else if (pageNow >= 3 && pageNow <= 8) {
+      pageNow = 4;
+      switchPage(pageNow);
+    } else if (pageNow >= 9 && pageNow <= 14) {
+      pageNow = 9;
+      switchPage(pageNow);
+    }
+    console.log("restart level");
+  }
+  if (
+    state === 3 &&
+    mouseX > 360 &&
+    mouseX < 460 &&
+    mouseY > 260 &&
+    mouseY < 310
+  ) {
+  }
+  //game Won
+  if (
+    state === 3 &&
+    mouseX > 240 &&
+    mouseX < 340 &&
+    mouseY > 260 &&
+    mouseY < 310
+  ) {
+    state = 0;
+    dog.live = 100;
+    dog.points = 0;
+    console.log("back to Menu");
+  }
+  //next level
+  if (
+    state === 3 &&
+    mouseX > 360 &&
+    mouseX < 460 &&
+    mouseY > 260 &&
+    mouseY < 310
+  ) {
+    state = 4;
+    dog.live = 100;
+    dog.x = 5;
+    pageNow += 1;
+    switchPage(pageNow);
+    console.log("next level");
+  }
+}
 
 function createPage(id) {
   page = {
     cats: [],
     humans: [],
     trees: [],
-    shrubbery: []
+    shrubbery: [],
+    holes: [],
+    liveBox: [],
+    doghouse: []
   };
   switch (id) {
     case 0:
-      page.shrubbery.push(spawnShrub(400, 400));
+      page.liveBox.push(spawnLiveBox(100, 350));
+      page.trees.push(spawnTree(230, 375));
+      page.shrubbery.push(spawnShrub(387, 450));
+      page.holes.push(spawnHole(440, 500));
       break;
     case 1:
-      page.cats.push(spawnCat(200, 472, 1));
+      page.shrubbery.push(spawnShrub(80, 450));
+      page.cats.push(spawnCat(200, 472, 2));
+      page.shrubbery.push(spawnShrub(230, 450));
+      page.shrubbery.push(spawnShrub(380, 450));
+      page.humans.push(spawnHuman(500, 433, 1));
+      page.shrubbery.push(spawnShrub(530, 450));
+      break;
+    case 2:
+      page.doghouse.push(spawnDoghouse(550, 428));
+      break;
+    //level 2
+    case 3:
+      break;
+
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    case 7:
+      break;
+    case 8:
+      page.doghouse.push(spawnDoghouse(550, 428));
+      break;
+    //level 3
+    case 9:
+      break;
+    case 10:
+      break;
+    case 11:
+      break;
+    case 12:
+      break;
+    case 13:
+      break;
+    case 14:
       page.cats.push(spawnCat(200, 472, 2));
       page.humans.push(spawnHuman(400, 433, 1));
       page.trees.push(spawnTree(450, 375));
-
-      break;
-    case 2:
-      page.cats.push(spawnCat(200, 472, 1));
-      break;
-    case 3:
+      page.holes.push(spawnHole(100, 500));
+      page.shrubbery.push(spawnShrub(400, 450));
+      page.liveBox.push(spawnLiveBox(100, 355));
+      page.doghouse.push(spawnDoghouse(550, 428));
       break;
   }
   level.page.push(page);
 }
 function drawPage() {
-  //ground
-  //rect(0, 650, 650, 5);
-  image(groundPNG, 0, 500, 650, 650, 0, 0, 800, 800);
+  if (state === 0) {
+    start();
+  } else if (state === 1) {
+    selectLevel();
+  } else if (state === 2) {
+    gameOver();
+  } else if (state === 3) {
+    gameWon();
+  } else if (state === 4) {
+    //sky
+    image(backgroundPNG, 0, 0, 650, 650, 0, 0, 800, 800);
+    //ground
+    //rect(0, 650, 650, 5);
+    image(groundPNG, 0, 500, 650, 650, 0, 0, 800, 800);
 
-  for (let i in page.cats) {
-    drawCat(page.cats[i]);
+    for (let i in page.cats) {
+      drawCat(page.cats[i]);
+    }
+    for (let i in page.humans) {
+      drawHuman(page.humans[i]);
+    }
+    for (let i in page.trees) {
+      drawTree(page.trees[i]);
+    }
+    for (let i in page.shrubbery) {
+      drawShrub(page.shrubbery[i]);
+    }
+    for (let i in page.holes) {
+      drawHole(page.holes[i]);
+    }
+    for (let i in page.liveBox) {
+      drawLiveBox(page.liveBox[i]);
+    }
+    for (let i in page.doghouse) {
+      drawDoghouse(page.doghouse[i]);
+    }
+
+    hit();
+    movePlayer();
+    drawPlayer();
+    drawLive();
+    drawPoints();
   }
-  for (let i in page.humans) {
-    drawHuman(page.humans[i]);
-  }
-  for (let i in page.trees) {
-    drawTree(page.trees[i]);
-  }
-  for (let i in page.shrubbery) {
-    drawShrub(page.shrubbery[i]);
-  }
+  drawLevelNr();
 }
 function updatePage() {
   for (let i = 0; i < page.cats.length; i++) {
@@ -486,10 +844,24 @@ function updatePage() {
 function switchPage(n) {
   page = level.page[n];
 }
+//level 1
 createPage(0);
 createPage(1);
 createPage(2);
+//level 2
 createPage(3);
+createPage(4);
+createPage(5);
+createPage(6);
+createPage(7);
+createPage(8);
+//level 3
+createPage(9);
+createPage(10);
+createPage(11);
+createPage(12);
+createPage(13);
+createPage(14);
 
 page = level.page[pageNow];
 function draw() {
@@ -497,6 +869,4 @@ function draw() {
 
   drawPage();
   updatePage();
-  movePlayer();
-  drawPlayer();
 }
--- js/p5setup.js
+++ js/p5setup.js
@@ -1,3 +1,17 @@
+let dog_spriteR;
+let dog_spriteL;
+let cat_spriteR;
+let cat_spriteL;
+let human_spriteR;
+let human_spriteL;
+let backgroundPNG;
+let groundPNG;
+let treePNG;
+let shrubPNG;
+let liveBoxPNG;
+let doghousePNG;
+let holePNG;
+
 function preload() {
   dog_spriteR = loadImage("png/dog-spritesheet.png");
   dog_spriteL = loadImage("png/dog-spritesheet2.png");
@@ -5,8 +19,13 @@ function preload() {
   cat_spriteL = loadImage("png/cat-spritesheetL.png");
   human_spriteR = loadImage("png/human-spritesheetR.png");
   human_spriteL = loadImage("png/human-spritesheetL.png");
+  backgroundPNG = loadImage("png/background.png");
   groundPNG = loadImage("png/ground.png");
   treePNG = loadImage("png/tree.png");
+  shrubPNG = loadImage("png/shrub.png");
+  liveBoxPNG = loadImage("png/liveBox.png");
+  doghousePNG = loadImage("png/doghouse.png");
+  holePNG = loadImage("png/hole.png");
 
   soundFormats("mp3", "ogg");
   //http://soundbible.com/tags-fireball.html
